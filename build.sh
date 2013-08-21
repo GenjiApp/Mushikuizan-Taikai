c28724b7507af5490f769c55f53ba591708e5064
Genji <chaos.nonlinear@gmail.com>
2013-08-21 13:10:42 +0900

    fix directory structure

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
+srcdir=src
 filename=mushikuizan_taikai
 epubcheck=/Applications/epubcheck-3.0.1/epubcheck-3.0.1.jar
 kindlegen=/Applications/KindleGen_Mac_i386_v2_9/kindlegen
-excludes="mimetype *.DS_Store $filename.epub $filename.mobi $filename.azk $0"
 
 case "$1" in
     "epub")
         if [ -e $filename.epub ]; then rm -f $filename.epub; fi
-        zip $filename.epub -X -0 mimetype
-        zip $filename.epub -r * -x $excludes ;;
+        pushd $srcdir && \
+            zip ../$filename.epub -X -0 mimetype && \
+            zip ../$filename.epub -r * -x mimetype *.DS_Store && \
+            popd ;;
     "check")
         if [ ! -e $filename.epub ]; then $0 epub; fi
         java -jar $epubcheck $filename.epub ;;
@@ -17,6 +19,7 @@ case "$1" in
         if [ ! -e $filename.epub ]; then $0 epub; fi
         $kindlegen -locale ja $filename.epub ;;
     "clean") rm -f *.epub *.mobi *.azk ;;
+    *) echo "Usage: $0 [epub|check|mobi|clean]" ;;
 esac
 
 exit 0
